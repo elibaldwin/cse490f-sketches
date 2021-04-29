c84b0d78bb5136ed07be4f62efc444482140ab24
Elias Baldwin <eliasab@cs.washington.edu>
2021-04-29 04:11:50 -0700

    final mp1 code

--- mp1_testing/RingBuffer.h
+++ mp1_testing/RingBuffer.h
@@ -1,18 +1,27 @@
 
-const int BUF_SIZE = 16;
+// Simple ring buffer implementation, using a template
+// to allow custom buffer sizes
 
+// (higher values of N will result in better smoothing, but also
+// increase response time for changing values)
+
+template <int N>
 class RingBuffer {
   private:
-    int buf[BUF_SIZE];
-    int sum;
-    int i;
+    int buf[N];
+    long sum;  // running sum of all buffer values
+    int i;     // 'pointer' to next slot in buffer
 
   public:
     int update_buf(int val) {
-      sum -= buf[i];
-      sum += val;
-      buf[i] = val;
-      i = (i+1) % BUF_SIZE;
-      return sum / BUF_SIZE;
+      sum -= buf[i];  // subtract the value in the next slot from the sum
+      sum += val;     // add the new value to the sum
+      buf[i] = val;   // put the new value in the next slot
+      i = (i+1) % N;  // increment slot pointer
+      return sum / N; // return average of all buffer values
+    }
+
+    int get() {
+      return sum / N;
     }
 };
--- mp1_testing/mp1_testing.ino
+++ mp1_testing/mp1_testing.ino
@@ -1,14 +1,18 @@
+#include <PushButton.h>
 #include "RingBuffer.h"
 #include "src/RGBConverter/RGBConverter.h"
-const boolean COMMON_ANODE = false; 
 
 const int POT_PIN = A0;
 
 // configuration values for calibrating potentiometer range
-const int POT_MIN = 220; 
+const int POT_MIN = 140; 
 const int POT_MAX = 960;
 
 const int LIGHTSENSE_PIN = A1;
+
+// configuration values for light sensor range
+const int LIGHT_MIN = 0;
+const int LIGHT_MAX = 250;
 
 // pins for the two parallel RGB LEDs
 const int RGB_R_PIN = 6;
@@ -19,11 +23,25 @@ const int RGB_B_PIN = 3;
 const int WHT1_PIN = 9;
 const int WHT2_PIN = 10;
 
+const int WHT_BRIGHTNESS = 180;
+
 // pin for the red button LED
 const int BTN_LED_PIN = 11;
+const int BTN_PIN = 12;
 
-RingBuffer smooth_buf;
+PushButton mode_btn(BTN_PIN);
+
+RingBuffer<16> pot_buf;
+RingBuffer<64> light_buf;
 RGBConverter rgb_conv;
+
+int mode = 0;
+const int N_MODES = 4;
+
+long last_update_millis = 0;
+const int DELAY = 20;
+
+const int FADE_INTERVAL = 5000;
 
 void setup() {
   // put your setup code here, to run once:
@@ -36,39 +54,88 @@ void setup() {
   pinMode(WHT1_PIN, OUTPUT);
   pinMode(WHT2_PIN, OUTPUT);
   pinMode(BTN_LED_PIN, OUTPUT);
+
+  pinMode(BTN_PIN, INPUT_PULLUP);
+  mode_btn.setActiveLogic(LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  int pot_val = smooth_buf.update_buf(analogRead(POT_PIN));
+  mode_btn.update();
+  if (mode_btn.isClicked()) {
+    mode = (mode+1) % N_MODES;
+  }
 
-  // set color of RBG LEDs based on pot position
-  float hue = constrain(pot_val - POT_MIN, 0, POT_MAX-POT_MIN) / (float)(POT_MAX-POT_MIN);
-  byte rgb[3];
-  rgb_conv.hslToRgb(hue, 1, 0.5, rgb);
-  setColor(rgb[0], rgb[1], rgb[2]);
+  if (millis() > last_update_millis + DELAY) {
+    last_update_millis = millis();
+    // read from analog inputs, updating ring buffers
+    int light_level = light_buf.update_buf(analogRead(LIGHTSENSE_PIN));
+    int pot_val = pot_buf.update_buf(analogRead(POT_PIN));
 
-  // set brightness of white LEDs based on pot position
-  int brightness = (int)(hue * 255);
-  analogWrite(WHT1_PIN, brightness);
-  analogWrite(WHT2_PIN, brightness);
-  analogWrite(BTN_LED_PIN, brightness);
-
-  int light_level = analogRead(LIGHTSENSE_PIN);
-
-  Serial.print("lightLevel=");
-  Serial.print(light_level);
-  Serial.print(" potVal=");
-  Serial.print(pot_val);
-  Serial.print(" hue=");
-  Serial.println(hue);
-
-  delay(5);
+    switch(mode) {
+      case 0: // MODE 0: OFF
+        set_hue(0, 0, true);
+        break;
+      case 1: // MODE 1: Crossfade w/ inversely proportional brightness
+        step_mode_1();
+        break;
+      case 2: // MODE 2: Rotation-based color selection
+        step_mode_2();
+        break;
+      case 3: // MODE 3: Rotation-based color breathing
+        step_mode_3();
+        break;
+    }
+  
+    Serial.print("mode=");
+    Serial.print(mode);
+    Serial.print(" lightLevel=");
+    Serial.print(light_level);
+    Serial.print(" potVal=");
+    Serial.println(pot_val);
+  }
+  
 }
 
-void setColor(int red, int green, int blue)
+void step_mode_1() {
+  float lightness = constrain(light_buf.get() - LIGHT_MIN, 0, LIGHT_MAX-LIGHT_MIN) / (float)(LIGHT_MAX-LIGHT_MIN);
+  float darkness = 1-lightness;
+
+  float hue = (millis() % FADE_INTERVAL) / (float)FADE_INTERVAL;
+
+  set_hue(hue, darkness, true);
+}
+
+void step_mode_2() {
+  float hue = constrain(pot_buf.get() - POT_MIN, 0, POT_MAX-POT_MIN) / (float)(POT_MAX-POT_MIN);
+  set_hue(hue, 1.0, true);
+}
+
+void step_mode_3() {
+  float hue = constrain(pot_buf.get() - POT_MIN, 0, POT_MAX-POT_MIN) / (float)(POT_MAX-POT_MIN);
+  float brightness = (sin(double(millis()) / (FADE_INTERVAL / 5.0)) + 1) / 2.0;
+
+  set_hue(hue, brightness, false);
+}
+
+// expects hue in    range [0.0, 1.0]
+// and brightness in range [0.0, 1.0]
+void set_hue(float hue, float brightness, boolean fade_whites)
 {
-  analogWrite(RGB_R_PIN, red);
-  analogWrite(RGB_G_PIN, green);
-  analogWrite(RGB_B_PIN, blue);  
+  byte rgb[3];
+  rgb_conv.hslToRgb(hue, 1, 0.5, rgb);
+  analogWrite(RGB_R_PIN, (int)(rgb[0] * brightness));
+  analogWrite(RGB_G_PIN, (int)(rgb[1] * brightness));
+  analogWrite(RGB_B_PIN, (int)(rgb[2] * brightness));
+
+  if (fade_whites) {
+    // update brightnesses of white LEDs and the button LED
+    analogWrite(WHT1_PIN, (int)(rgb[0] * brightness));
+    analogWrite(WHT2_PIN, (int)(rgb[1] * brightness));
+    analogWrite(BTN_LED_PIN, (int)(rgb[2] * brightness));
+  } else {
+    analogWrite(WHT1_PIN, (int)(WHT_BRIGHTNESS * brightness));
+    analogWrite(WHT2_PIN, (int)(WHT_BRIGHTNESS * brightness));
+    analogWrite(BTN_LED_PIN, (int)(WHT_BRIGHTNESS * brightness));
+  }
+  
 }
